658d9edc49f4ec7b497dfe824ebddd35a5e187db
Jared Wasinger <j-wasinger@hotmail.com>
2024-08-24 10:27:18 -0700

    bench script working now for all benchmarks

--- make_and_bench.sh
+++ make_and_bench.sh
@@ -4,11 +4,14 @@ bench_tmp_file=$(mktemp)
 noop_tmp_file=$(mktemp)
 
 python3 build_benchmark_contract.py templates/benchmark.huff.tmpl bench > contracts/benchmark.huff
-python3 build_benchmark_contract.py templates/benchmark.huff.tmpl bench > contracts/noop.huff
+python3 build_benchmark_contract.py templates/benchmark.huff.tmpl noop > contracts/noop.huff
 
 huffc --bytecode contracts/benchmark.huff -r 2>&1 | python3 capture_huffc_output.py > $bench_tmp_file
 huffc --bytecode contracts/noop.huff -r 2>&1 | python3 capture_huffc_output.py > $noop_tmp_file
 
 input=$(python3 gen_bench_input.py $PRECOMPILE $INPUT_COUNT)
 
-~/projects/go-ethereum/build/bin/evm --codefile $bench_tmp_file --input $input --prestate ./genesis.json --bench run 2>&1
+bench_stats=$(~/projects/go-ethereum/build/bin/evm --codefile $bench_tmp_file --input $input --prestate ./genesis.json --bench run 2>&1 | python3 calc_stats.py)
+noop_stats=$(~/projects/go-ethereum/build/bin/evm --codefile $noop_tmp_file --input $input --prestate ./genesis.json --bench run 2>&1 | python3 calc_stats.py)
+
+echo "$noop_stats,$bench_stats" | python3 measure_perf.py
--- measure_perf.py
+++ measure_perf.py
@@ -1,24 +1,22 @@
 import sys
 
-lines = sys.stdin.readlines()
+input = sys.stdin.read()
 
-exec_time = 0
-gas_used = 0
+stats_noop = input.split(',')[0]
+noop_exec_time = int(stats_noop.split("-")[0])
+noop_gas_used = int(stats_noop.split("-")[1])
 
-for line in lines:
-	if "execution time:" in line:
-		exec_time = line.strip('execution time:  ').rstrip('\n')
-		if exec_time.endswith("ms"):
-			exec_time = int(float(exec_time[:-2]) * 1000000)
-		elif exec_time.endswith("\\xc2\\xb5s"):
-			exec_time = int(float(exec_time[:-9]) * 1000)
-		elif exec_time.endswith("s"):
-			exec_time = int(float(exec_time[:-1]) * 1000000 * 1000)
-		else:
-			raise Exception("unknown timestamp ending: {}".format(exec_time))
-	elif "EVM gas used:" in line:
-		gas_used = int(line.strip("EVM gas used:").strip('\\n'))
+stats_bench = input.split(',')[1]
+bench_exec_time = int(stats_bench.split("-")[0])
+bench_gas_used = int(stats_bench.split("-")[1])
 
-gas_throughput = gas_used / (exec_time / 1e9)
+print("bench gas used: {}, bench time: {}".format(bench_gas_used, bench_exec_time))
+print("noop gas used: {}, noop time: {}".format(noop_gas_used, noop_exec_time))
+
+# calc throughput after subtracting noop gas/time from bench gas/time
+corrected_gas_used = bench_gas_used - noop_gas_used
+corrected_exec_time = bench_exec_time - noop_exec_time
+
+gas_throughput = corrected_gas_used / (corrected_exec_time / 1e9)
 gas_throughput /= 1e6
 print("throughput: {} million gas / sec".format(gas_throughput))
